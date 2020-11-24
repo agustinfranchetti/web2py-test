96bf4fdb8e677d5bcf8ef16e8a7fcc9f0b9ae392
agustinfranchetti <agustinfranchetti@gmail.com>
2020-11-24 17:17:22 -0300

    show names for boss dropdown

--- models/db_contacts.py
+++ models/db_contacts.py
@@ -10,10 +10,12 @@ db.define_table('employee',
                 Field('name'),
                 Field('email'),
                 Field('age', 'integer'),
-                Field('id_boss','reference employee')
+                Field('id_boss','reference employee',
+                     requires=IS_EMPTY_OR(IS_IN_DB(db, 'employee.id','%(name)s')))
                 )
 
 db.define_table('proyectos',
                 Field('id_proyecto'),
-                Field('legajo_propietario', 'reference students')
+                Field('legajo_propietario', 'reference students',
+                     requires=IS_EMPTY_OR(IS_IN_DB(db, 'students.legajo','%(legajo)s')))
                 )
